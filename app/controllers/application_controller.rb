98a0e5aad1aef606d7f742c09b7ca99bf37c4bdb
Prian Kuhanandan <priankuhan@gmail.com>
2015-06-16 19:42:34 +0000

    Use regular HTTP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-
+before_filter do
+    if request.ssl? && Rails.env.production?
+      redirect_to :protocol => 'http://', :status => :moved_permanently
+    end
+  end
   
 end
 
