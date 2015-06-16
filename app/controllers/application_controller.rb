668bb328ec28ef543b7d98d982b40df7f2e363e7
Prian Kuhanandan <priankuhan@gmail.com>
2015-06-16 19:46:59 +0000

    Revert force normal http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-before_filter do
-    if request.ssl? && Rails.env.production?
-      redirect_to :protocol => 'http://', :status => :moved_permanently
-    end
-  end
   
 end
 
